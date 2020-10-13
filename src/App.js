00af02d589ae80819321cf7798c32ca6ab4ae436
anais-ctnt <anaiscotinet@gmail.com>
2020-10-13 11:47:58 +0200

    adding users

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact'
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact 
-      name="Claudia Chambers"
-      avatar="https://randomuser.me/api/portraits/women/67.jpg"
-      isOnline />
-
-      <Contact 
-      name="Margie Spencer"
-      avatar="https://randomuser.me/api/portraits/women/79.jpg"
-      isOnline />
-
-      <Contact 
-      name="Steven Vargas"
-      avatar="https://randomuser.me/api/portraits/men/83.jpg" />
+      <ContactList/>
     </div>
   );
 }
